60052d3d5f396af08d18eb3f16b0b602dfcc579a
nursultan.sagynbaev@alatoo.edu.kg <nursultan.sagynbaev@alatoo.edu.kg>
2023-12-13 02:41:18 +0600

    New Design

--- screens/profile.js
+++ screens/profile.js
@@ -1,55 +1,89 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
 const Profile = () => {
     return ( <
-        View style = { styles.container } >
+        View style = { styles.container } > { /* Header with Profile Picture */ } <
+        View style = { styles.header } >
         <
         Image style = { styles.profileImage }
         source = {
             { uri: 'https://placekitten.com/200/200' }
-        }
+        } // Replace with the URL of your profile picture
         /> <
         Text style = { styles.name } > John Doe < /Text> <
-        Text style = { styles.bio } > Passionate Developer < /Text> < /
+        Text style = { styles.major } > Computer Science Major < /Text> < /
         View >
+
+        { /* Student Information */ } <
+        View style = { styles.studentInfo } >
+        <
+        Text style = { styles.label } > University: < /Text> <
+        Text style = { styles.value } > Awesome University < /Text>
+
+        <
+        Text style = { styles.label } > Batch: < /Text> <
+        Text style = { styles.value } > 2023 < /Text>
+
+        <
+        Text style = { styles.label } > GPA: < /Text> <
+        Text style = { styles.value } > 3.8 < /Text> < /
+        View >
+
+        { /* Edit Profile Button */ } <
+        TouchableOpacity style = { styles.editButton } >
+        <
+        Text style = { styles.editButtonText } > Edit Profile < /Text> < /
+        TouchableOpacity > <
+        /View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#fff',
+    },
+    header: {
         alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#f0f0f0',
+        paddingVertical: 20,
+        backgroundColor: '#3498db',
     },
     profileImage: {
-        width: 150,
-        height: 150,
-        borderRadius: 75,
-        marginBottom: 20,
-    },
-    userInfo: {
-        alignItems: 'center',
-        marginBottom: 20,
+        width: 120,
+        height: 120,
+        borderRadius: 60,
+        marginBottom: 10,
     },
     name: {
         fontSize: 24,
         fontWeight: 'bold',
+        color: '#fff',
+    },
+    major: {
+        fontSize: 16,
+        color: '#fff',
+    },
+    studentInfo: {
+        paddingHorizontal: 20,
+        marginTop: 20,
+    },
+    label: {
+        fontSize: 16,
+        color: '#333',
         marginBottom: 5,
     },
-    bio: {
-        fontSize: 16,
-        color: 'gray',
-        marginBottom: 10,
+    value: {
+        fontSize: 18,
+        marginBottom: 15,
     },
     editButton: {
-        backgroundColor: '#3498db',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 10,
+        backgroundColor: '#27ae60',
+        paddingVertical: 15,
+        alignItems: 'center',
     },
     editButtonText: {
-        color: 'white',
+        color: '#fff',
         fontWeight: 'bold',
     },
 });
