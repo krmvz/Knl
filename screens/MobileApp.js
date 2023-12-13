43746874ab5ba845eef8399ac0fe8d24467c6592
Bekmamat Tashmatov <rakuzanb23@gmail.com>
2023-12-13 15:02:19 +0600

    changed some design in python page

--- screens/MobileApp.js
+++ screens/MobileApp.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
-import { WebView } from 'react-native-webview';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image } from 'react-native';
 import { Linking } from 'react-native';
 
 const MobileApp = () => {
   const videoUrl = 'https://www.youtube.com/watch?v=f1f3d2gVadU&list=PLsO_V9s8C6fqtib6olJE4F-MvTW-hNWa6';
 
   const chapters = [
-    { title: 'Chapter One♠', content: 'Introduction to Mobile App Development' },
-    { title: 'Chapter Two♠', content: 'Building User Interfaces with React Native' },
-    { title: 'Chapter Three♠', content: 'State Management in React Native' },
+    {
+      title: 'Chapter One♠',
+      content: 'Introduction to Mobile App Development Mobile app development involves creating software applications that are designed to run on mobile devices such as smartphones and tablets. These applications can be developed for various mobile operating systems, including iOS (Apple), Android (Google), and sometimes Windows or other platforms.',
+      imageUrl: 'https://www.collatree.com/wp-content/uploads/2021/03/Social-Media-Poster-Collatree-1.png',
+    },
+    {
+      title: 'Chapter Two♠',
+      content: 'Building User Interfaces with React Native Building user interfaces with React Native involves creating cross-platform mobile applications using the React framework. React Native allows developers to use a single codebase to build applications for both iOS and Android platforms.',
+      imageUrl: 'https://media.licdn.com/dms/image/D5612AQEbJdFM_VZPJQ/article-cover_image-shrink_720_1280/0/1693024748643?e=2147483647&v=beta&t=qgIqKTAoIlNt2MsHugcfHrJ0zXgB7F0BOJ4H2Uqf-f8',
+    },
+    {
+      title: 'Chapter Three♠',
+      content: 'State Management in React Native State management is a crucial aspect of React Native development as it allows you to manage and update the state of your application. There are several approaches to state management in React Native, and the choice often depends on the complexity of your application. ',
+      imageUrl: 'https://www.loginradius.com/blog/static/ef27359624480604211f0e018dd47f18/d9e4b/title-image.png',
+    },
     // Add more chapters as needed
   ];
 
@@ -23,6 +34,9 @@ const MobileApp = () => {
         <View key={index}>
           <Text style={styles.chapterTitle}>{chapter.title}</Text>
           <View style={styles.divider} />
+          {chapter.imageUrl && (
+            <Image source={{ uri: chapter.imageUrl }} style={styles.chapterImage} />
+          )}
           <Text style={styles.chapterContent}>{chapter.content}</Text>
 
           {index === 0 && (
@@ -31,10 +45,11 @@ const MobileApp = () => {
             </TouchableOpacity>
           )}
 
-          <WebView
-            source={{ html: `<iframe width="100%" height="200" src="${videoUrl}" frameborder="0" allowfullscreen></iframe>` }}
-            style={styles.webView}
-          />
+          {index === 1 || index === 2 ? (
+            <TouchableOpacity onPress={openVideoLink} style={styles.videoLink}>
+              <Text style={styles.videoLinkText}>Watch Video</Text>
+            </TouchableOpacity>
+          ) : null}
         </View>
       ))}
     </ScrollView>
@@ -50,6 +65,11 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 8,
+  },
+  chapterImage: {
+    height: 200,
+    resizeMode: 'cover',
+    marginBottom: 16,
   },
   divider: {
     borderBottomColor: 'black',
@@ -70,9 +90,6 @@ const styles = StyleSheet.create({
   videoLinkText: {
     color: 'white',
     fontWeight: 'bold',
-  },
-  webView: {
-    height: 200,
   },
 });
 
--- screens/Python.js
+++ screens/Python.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
-import { WebView } from 'react-native-webview';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image } from 'react-native';
 import { Linking } from 'react-native';
 
 const PythonOOP = () => {
-  const videoUrl = 'https://youtu.be/Ej_02ICOIgs?si=kGyVTyQ5v_y9xEXi';
+  const videoUrl = 'https://youtu.be/Ej_02ICOIgs?si=AE6WTJMTcy7uGrk3';
 
   const chapters = [
-    { title: 'Chapter One♠', content: 'Introduction to  Python OOP Development' },
-    { title: 'Chapter Two♠', content: 'CLASS Variables' },
-    { title: 'Chapter Three♠', content: 'Class and Static Method' },
+    {
+      title: 'Chapter One♠',
+      content: 'Introduction to Pytjon OOP                                         Object-Oriented Programming (OOP) is a programming paradigm that uses objects and classes for organizing code. Python, being an object-oriented programming language, supports OOP principles.',
+      imageUrl: 'https://www.boardinfinity.com/blog/content/images/2023/03/Copy-of-Copy-of-Copy-of-Untitled-Design--1--1.png',
+    },
+    {
+      title: 'Chapter Two♠',
+      content: 'CLASS Variables                                                                                                     In Python, class variables are variables that are shared among all instances of a class. Unlike instance variables, which are unique to each instance of a class, class variables are the same for all instances.',
+      imageUrl: 'https://i2.wp.com/www.differencebetween.com/wp-content/uploads/2018/02/Difference-Between-Class-and-Instance-Variables-fig-1.png?resize=550%2C347&ssl=1',
+    },
+    {
+      title: 'Chapter Three♠',
+      content: 'Class and Static Method                                                In Python, class methods and static methods are special types of methods that are associated with a class rather than an instance of the class. They are defined using the @classmethod and @staticmethod decorators, respectively.',
+      imageUrl: 'https://www.boardinfinity.com/blog/content/images/2023/05/Class-and-Static-method-in-Python.png',
+    },
     // Add more chapters as needed
   ];
 
@@ -23,6 +34,9 @@ const PythonOOP = () => {
         <View key={index}>
           <Text style={styles.chapterTitle}>{chapter.title}</Text>
           <View style={styles.divider} />
+          {chapter.imageUrl && (
+            <Image source={{ uri: chapter.imageUrl }} style={styles.chapterImage} />
+          )}
           <Text style={styles.chapterContent}>{chapter.content}</Text>
 
           {index === 0 && (
@@ -31,10 +45,11 @@ const PythonOOP = () => {
             </TouchableOpacity>
           )}
 
-          <WebView
-            source={{ html: `<iframe width="100%" height="200" src="${videoUrl}" frameborder="0" allowfullscreen></iframe>` }}
-            style={styles.webView}
-          />
+          {index === 1 || index === 2 ? (
+            <TouchableOpacity onPress={openVideoLink} style={styles.videoLink}>
+              <Text style={styles.videoLinkText}>Watch Video</Text>
+            </TouchableOpacity>
+          ) : null}
         </View>
       ))}
     </ScrollView>
@@ -50,6 +65,11 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 8,
+  },
+  chapterImage: {
+    height: 200,
+    resizeMode: 'cover',
+    marginBottom: 16,
   },
   divider: {
     borderBottomColor: 'black',
@@ -70,9 +90,6 @@ const styles = StyleSheet.create({
   videoLinkText: {
     color: 'white',
     fontWeight: 'bold',
-  },
-  webView: {
-    height: 200,
   },
 });
 
